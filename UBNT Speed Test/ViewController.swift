5bd8867f3cdbde512535c2edd5c1cfcdff9b3794
Roman Kříž <roman@kriz.io>
2018-03-20 15:01:41 +0100

    Initial Commit

---
+++ UBNT Speed Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UBNT Speed Test
+//
+//  Created by Roman Kříž on 20/03/2018.
+//  Copyright © 2018 Roman Kříž. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
